bc446cf7be728498e4afc6a7c12e2fe40aae034c
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-07-11 20:09:18 +0200

    Send function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
+use anyhow::Result;
+use serenity::{
+    http::Http,
+    model::{
+        channel::Embed,
+        webhook::Webhook,
+    },
+};
+
 mod consts;
 
 fn main() {
-    let webhook = consts::WEBHOOK;
-    println!("{webhook}");
+  
+}
+
+async fn send(data: String) -> Result<()> {
+    let http = Http::new("token");
+    let url = consts::WEBHOOK;
+
+    // Create webhook.
+    let webhook = Webhook::from_url(&http, url).await?;
+
+    // Embed [data] in a discord message.
+    let embed = Embed::fake(|e| {
+        e.title("System info");
+        e.description(data);
+        e
+    });
+
+    // Execute Webhook, add metadata, content and files.
+    webhook
+        .execute(&http, true, |w| {
+            w.content("content")
+                .username("username")
+                .embeds(vec![embed]);
+            w
+        })
+        .await?;
+    Ok(())
 }
